9e41125df3c9e70e26c525a78745b72e918a9142
starglow-it <starglowventures@gmail.com>
2024-10-11 16:07:11 -0400

    first commit

---
+++ lib/heritage_tool.ex
@@ -0,0 +1,18 @@
+defmodule HeritageTool do
+  @moduledoc """
+  Documentation for `HeritageTool`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> HeritageTool.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/heritage_tool_test.exs
@@ -0,0 +1,8 @@
+defmodule HeritageToolTest do
+  use ExUnit.Case
+  doctest HeritageTool
+
+  test "greets the world" do
+    assert HeritageTool.hello() == :world
+  end
+end
